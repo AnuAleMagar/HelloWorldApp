bda5cad1f45b55fcf2f17bdf78e12bca328205ae
Anu Ale Magar <anualemagar212@gmail.com>
2024-06-30 21:04:08 +0545

    page_count page added

--- app.py
+++ app.py
@@ -1,17 +1,26 @@
 from flask import Flask,render_template,request,url_for,redirect
 from markupsafe import escape
 app=Flask(__name__)
+count=0
+def increment_count():
+     global count
+     count+=1
+
 @app.route('/')
 def index():
-    return render_template("index.html")
+    increment_count()
+    return  render_template("index.html")
 @app.route('/about')
 def about():
+    increment_count()
     return render_template("about.html")
 @app.route('/contact')
 def contact():
+    increment_count()
     return render_template("contact.html")
 @app.route('/login')
 def login():
+    increment_count()
     username=request.args.get('username')
     if username:
          return redirect(url_for('loginsuccess',username=username))
@@ -20,6 +29,9 @@ def login():
 @app.route('/login/<username>')
 def loginsuccess(username):
     return f'Hello {escape(username)}, Welcome to my app'
+@app.route('/pagevisitcount')
+def page():
+    return f'Hello, Youhave visited this websites {escape(count)} times.'
 
 if __name__ == '__main__':
          app.run(debug=False)
